5e0cd3f6176a0e284c90bd7a40c4897bcb6db01c
Sally <sllzhao@ucdavis.edu>
2020-06-08 17:00:34 -0700

    Updates

---
+++ precache-manifest.73f7d4bdcc80a23a30a283f53a72e73b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aeb3948c79bc88d5ece484910abe579d",
+    "url": "/awsociety/index.html"
+  },
+  {
+    "revision": "a5b8453400eef8caa1c1",
+    "url": "/awsociety/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "3ddd29489f882108a4c2",
+    "url": "/awsociety/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a5b8453400eef8caa1c1",
+    "url": "/awsociety/static/js/2.aec46558.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/awsociety/static/js/2.aec46558.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ddd29489f882108a4c2",
+    "url": "/awsociety/static/js/main.30ae18f4.chunk.js"
+  },
+  {
+    "revision": "3dbd12ef3f6086deff76",
+    "url": "/awsociety/static/js/runtime-main.b6ed97b4.js"
+  },
+  {
+    "revision": "4ce770912d01f7e85882d6eb4f3cc0ef",
+    "url": "/awsociety/static/media/animal.4ce77091.jpg"
+  }
+]);
