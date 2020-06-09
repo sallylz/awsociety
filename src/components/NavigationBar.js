69dcdf8635d3717f4aee7e95279d73f9425e6477
Sally <sllzhao@ucdavis.edu>
2020-06-08 18:35:39 -0700

    update paths

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -28,8 +28,8 @@ export const NavigationBar = () => (
                 {/* margain left auto */}
                 <Nav className="ml-auto">
                     <Nav.Item><Nav.Link href="/">Home</Nav.Link></Nav.Item>
-                    <Nav.Item><Nav.Link href="/about">About</Nav.Link></Nav.Item>
-                    <Nav.Item><Nav.Link href="/contact">Contact</Nav.Link></Nav.Item>
+                    <Nav.Item><Nav.Link href="/awsociety/about">About</Nav.Link></Nav.Item>
+                    <Nav.Item><Nav.Link href="/awsociety/contact">Contact</Nav.Link></Nav.Item>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
